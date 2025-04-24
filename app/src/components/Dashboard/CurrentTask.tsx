8f3fb43927575d457783fadef281bb24029a9c79
DUDEbehindDUDE <jnbrooks457@gmail.com>
2025-04-24 02:44:23 -0400

    Show extendedStatus under current task in dashboard

--- app/src/components/Dashboard/CurrentTask.tsx
+++ app/src/components/Dashboard/CurrentTask.tsx
@@ -1,6 +1,16 @@
 import { Box, Typography } from "@mui/material";
+import { useContext } from "react";
+import { GlobalStatusContext } from "../Providers/ROSProvider";
 
+/**
+ * "Current Task" section of the dashboard, displays the extended
+ * status of the latest status message (or "Unknown if that is
+ * undefined"). The latest status should be the last item in the
+ * `globalStatusHistory` array.
+ */
 function CurrentTask() {
+  const { globalStatusHistory } = useContext(GlobalStatusContext);
+
   return (
     <Box
       sx={{
@@ -11,7 +21,9 @@ function CurrentTask() {
       }}
     >
       <Typography variant="h6">Current Task</Typography>
-      <Typography>Unknown</Typography>
+      <Typography>
+        {globalStatusHistory.at(-1)?.extendedStatus ?? "Unknown"}
+      </Typography>
     </Box>
   );
 }
