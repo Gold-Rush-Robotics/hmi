213041ce27fb01283d4c7008bd275570a5c5a1db
Justin Brooks <jnbrooks457@gmail.com>
2026-02-05 19:32:05 -0500

    Don't warn unused vars prefixed with _

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -39,7 +39,11 @@ export default tseslint.config(
       ],
       "@typescript-eslint/no-unused-vars": [
         "warn",
-        { argsIgnorePattern: "^_" },
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
       ],
       "@typescript-eslint/no-misused-promises": [
         "error",
